911dffe30d978668c631f127dafbb26d8ec595c9
Madhumanu891 <dhanavenimadhu23@gmail.com>
2025-05-04 22:12:26 +0530

    Initial commit

---
+++ Frontend/src/comp/Nav.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import Ct from './Ct'
+
+const Nav = () => {
+  let obj=useContext(Ct)
+  return (
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/reg">Studentregistration</Link>
+        <Link to="/hal">Print Hallticket</Link>
+     { obj.store.token==""&&<Link to="/login">AdminLogin</Link>}
+     { obj.store.token!=""&&  <Link to="/disp">Display</Link>}
+     { obj.store.token!=""&&  <Link to="/logout">Logout</Link>}
+     { obj.store.token!=""&& <div>{obj.store.name}</div>}
+    </nav>
+  )
+}
+
+export default Nav
